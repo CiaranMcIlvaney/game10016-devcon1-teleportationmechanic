9b86ec80fa1d25284d35fed0f0409d6bbff87d16
James-Belle <jamesbelle549@gmail.com>
2025-09-20 19:30:25 -0400

    finished the portal code.
    made it so that going into a portal teleports you to the other portal and vice versa, also made it so you can't place portals onto themselves

--- DevCon1_TeleportationMechanic/Assets/Scripts/teleportation.cs
+++ DevCon1_TeleportationMechanic/Assets/Scripts/teleportation.cs
@@ -7,58 +7,94 @@ using UnityEngine.UIElements;
 
 public class teleportation : MonoBehaviour
 {
+    // Alice/JamesBellefeuille
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { // this gets portal potitions
+        portal1Pos = portal1.GetComponent<Transform>().position;
+        portal2Pos = portal2.GetComponent<Transform>().position;
     }
-    Vector3 Portal1Pos = new Vector3 (0, 0, 0);
-    Vector3 Portal2Pos = new Vector3(0, 0, 0);
+    Vector3 portal1Pos;
+    Vector3 portal2Pos;
+
+    public Camera Cam; // this imports the camera
+
     public Object portal1;
     public Object portal2;
-    public bool tempRage = false;
-    public Camera Cam;
-    Vector3 newPortalArea = Vector3.up;
+
+    
+    bool portal1Placed = false; //this is to track if the portals have been placed
+    bool portal2Placed = false;
+
+    bool inPortal1 = false; // this is to track if the player is in a portal
+    bool inPortal2 = false;
+
     // Update is called once per frame
     void Update()
     {
+        // This checks if the player presses a button and if so runs portal placment
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            PortalPlacment();
+        }
+    }
+    public void PortalPlacment()
+    { 
+        // this fuction places the portals
         RaycastHit hit;
-        Ray angry = Cam.ScreenPointToRay(Input.mousePosition);
-        //get raycast from camera, 
-        //Debug.Log("working");
-        //if(Physics.Raycast)
-        if (Physics.Raycast(angry, out hit))
-        {
+        Ray cast = Cam.ScreenPointToRay(Input.mousePosition);
 
-            newPortalArea = hit.point;
-            //Debug.Log(hit.point);
-
+        if (Physics.Raycast(cast, out hit))
+        { // this checks if the raycast hit and if so it save the position
+            Vector3 newPortalArea = hit.point; // this is where the new portal will be placed
+            if (Input.GetMouseButtonDown(0))
+            { // if you press the left mouse button this places a portal
+                portal1Pos = newPortalArea;
+                portal1.GetComponent<Transform>().position = portal1Pos;
+                portal1Placed = true;
+            }
+            else if (Input.GetMouseButtonDown(1))
+            { // if you press the right mouse button this places a portal
+                portal2Pos = newPortalArea;
+                portal2.GetComponent<Transform>().position = portal2Pos;
+                portal2Placed = true;
+            }
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            Portal1Pos = newPortalArea;
-            //newPortalArea
-            portal1.GetComponent<Transform>().position = Portal1Pos;
-            //Debug.Log("yagsidhabsakfdjb");
-
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            Portal2Pos = newPortalArea;
-            //newPortalArea
-            portal2.GetComponent<Transform>().position = Portal2Pos;
-            //Debug.Log("yagsidhabsakfdjb");
-        }
-    }
-    
-    private void OnCollisionEnter(Collision collision)
-    {
-        //teleport player to other box
-        //if collides with box 1: teleport player to box 2
-        // else if collides with box 2: teleport player to box 1
-    }
-    public void OnMouseDown()
-    {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    { // this is for when you enter a portal to teleport the player to the other portal
+        bool portalsPlaced = portal1Placed && portal2Placed;
+        if (!inPortal1 && !inPortal2) // this checks that the player isnt in a portal atm
+        {
+            if (other.name == "portal1" && portalsPlaced)
+            { // if your in the first portal this teleports you to the second one
+                this.transform.position = portal2Pos;
+                inPortal2 = true; // after going through a portal this makes it so you wont teleport back and forth constantly
+
+            }
+            else if (other.name == "portal2" && portalsPlaced)
+            { // if your in the second portal this teleports you to the first one
+                this.transform.position = portal1Pos;
+                inPortal1 = true;
+            }
+        }
+        
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        // this renables the portals when you leave a portal
+        if (other.name == "portal1")
+        {
+            inPortal1 = false;
+
+        }
+        else if (other.name == "portal2")
+        {
+            inPortal2 = false;
+        }
+
     }
 }
